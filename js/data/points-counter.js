26f137c2aab9bf13e6d072c52a8ccc5ca7d020c4
OleksandrLz <4oleksandrlz@gmail.com>
2018-06-07 00:03:05 +0300

    add variables to point counter

--- js/data/points-counter.js
+++ js/data/points-counter.js
@@ -1,16 +1,24 @@
+const minRightAnswers = 10;
+const rightAnswerPoints = 100;
+const fastAnswerPoints = 50;
+const slowAnswerPoints = 50;
+const fastAnswerTime = 9;
+const slowAnswerTime = 15;
+const lifeBonus = 50;
+
 const countPoints = (answers, lives) => {
   let sum = 0;
-  if (answers.length < 10) {
+  if (answers.length < minRightAnswers) {
     sum = -1;
   } else {
     answers.forEach((el) => {
       if (el.correctAnswer) {
-        sum += 100;
-        sum += el.answerTime < 9 ? 50 : 0;
-        sum -= el.answerTime > 15 ? 50 : 0;
+        sum += rightAnswerPoints;
+        sum += el.answerTime < fastAnswerTime ? fastAnswerPoints : 0;
+        sum -= el.answerTime > slowAnswerTime ? slowAnswerPoints : 0;
       }
     });
-    sum += lives * 50;
+    sum += lives * lifeBonus;
   }
   return sum;
 };
